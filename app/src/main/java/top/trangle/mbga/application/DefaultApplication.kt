e8f00e48a46c6f85a13c653756ad98cfed9e8c4f
Cledwyn Lew <me@trangle.top>
2024-01-26 22:40:17 +0800

    feat: init

---
+++ app/src/main/java/top/trangle/mbga/application/DefaultApplication.kt
@@ -0,0 +1,11 @@
+package top.trangle.mbga.application
+
+import com.highcapable.yukihookapi.hook.log.YLog
+import com.highcapable.yukihookapi.hook.xposed.application.ModuleApplication
+
+class DefaultApplication : ModuleApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
