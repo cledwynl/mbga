d23525508109497ed6360d9e5d6a711571636a85
Cledwyn Lew <me@trangle.top>
2024-01-30 22:34:27 +0800

    feat: 更纯粹的分享链接

--- app/src/main/java/top/trangle/mbga/hook/VideoDetailHooker.kt
+++ app/src/main/java/top/trangle/mbga/hook/VideoDetailHooker.kt
@@ -1,6 +1,7 @@
 package top.trangle.mbga.hook
 
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
+import com.highcapable.yukihookapi.hook.factory.field
 import com.highcapable.yukihookapi.hook.factory.method
 
 object VideoDetailHooker : YukiBaseHooker() {
@@ -18,6 +19,25 @@ object VideoDetailHooker : YukiBaseHooker() {
                 if (prefs.getBoolean("vid_detail_disable_label")) {
                     resultNull()
                 }
+            }
+        }
+
+        val clzShareResult = "com.bilibili.lib.sharewrapper.online.api.ShareClickResult".toClass()
+        val contentField = clzShareResult.field { name = "content" }
+
+        val clzShareTargetTask =
+            "com.bilibili.app.comm.supermenu.share.v2.ShareTargetTask\$f".toClass()
+        val shareTaskCallback = clzShareTargetTask.method { name = "l" }
+
+        "ah1.c".toClass().method { name = "c" }.hook {
+            replaceUnit {
+                if (!prefs.getBoolean("vid_detail_disable_short_link")) {
+                    callOriginal()
+                    return@replaceUnit
+                }
+                val result = clzShareResult.getDeclaredConstructor().newInstance()
+                contentField.get(result).set(args[7])
+                shareTaskCallback.get(args[17]).invoke<Any>(result)
             }
         }
     }
