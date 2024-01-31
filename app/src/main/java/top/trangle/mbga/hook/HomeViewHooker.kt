8a85bc60427733a07c9a3cf811aa1dfd23be1bf7
Cledwyn Lew <me@trangle.top>
2024-01-30 20:06:44 -0600

    feat: 允许禁用首页部分tab

--- app/src/main/java/top/trangle/mbga/hook/HomeViewHooker.kt
+++ app/src/main/java/top/trangle/mbga/hook/HomeViewHooker.kt
@@ -1,6 +1,7 @@
 package top.trangle.mbga.hook
 
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
+import com.highcapable.yukihookapi.hook.factory.field
 import com.highcapable.yukihookapi.hook.factory.method
 import com.highcapable.yukihookapi.hook.log.YLog
 import top.trangle.mbga.utils.reflectionToString
@@ -23,6 +24,29 @@ object HomeViewHooker : YukiBaseHooker() {
                     }
                 }
                 YLog.debug("item: ${reflectionToString(instance)}")
+            }
+        }
+
+        val clzTabItem = "tv.danmaku.bili.ui.main2.basic.BaseMainFrameFragment\$s".toClass()
+        val fieldC = clzTabItem.field { name = "c" }
+
+        val clzL = "tv.danmaku.bili.ui.main2.resource.l".toClass()
+        val fieldD = clzL.field { name = "d" }
+
+        "tv.danmaku.bili.ui.main2.MainFragment\$a".toClass().method { name = "a" }.hook {
+            after {
+                (result as ArrayList<*>).removeIf { tabItem ->
+                    val c = fieldC.get(tabItem).any()
+                    val d = fieldD.get(c).string()
+
+                    val disable_home =
+                        prefs.getBoolean("tabs_disable_home") &&
+                            "bilibili://main/home".equals(d)
+                    val disable_dynamic =
+                        prefs.getBoolean("tabs_disable_dynamic") &&
+                            "bilibili://following/home".equals(d)
+                    disable_home || disable_dynamic
+                }
             }
         }
     }
