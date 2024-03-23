50c5e1555018a00bf7862c15e2d0217bd71dbba1
Cledwyn Lew <me@trangle.top>
2024-03-23 22:51:14 +0800

    feat: 干掉评论内容上的投票信息

--- app/src/main/java/top/trangle/mbga/hook/VideoCommentHooker.kt
+++ app/src/main/java/top/trangle/mbga/hook/VideoCommentHooker.kt
@@ -1,5 +1,7 @@
 package top.trangle.mbga.hook
 
+import com.highcapable.yukihookapi.hook.core.YukiMemberHookCreator
+import com.highcapable.yukihookapi.hook.core.api.priority.YukiHookPriority
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
 import com.highcapable.yukihookapi.hook.factory.method
 
@@ -24,13 +26,19 @@ object VideoCommentHooker : YukiBaseHooker() {
     }
 
     private fun hookVote() {
-        "com.bilibili.app.comment.ext.widgets.CmtVoteWidget".toClass()
-            .method { name = "a" }.hook {
-                replaceUnit {
-                    if (!prefs.getBoolean("vid_comment_no_vote")) {
-                        callOriginal()
-                    }
+        val replaceHook: (YukiMemberHookCreator.MemberHookCreator) -> Unit = {
+            it.replaceUnit {
+                if (!prefs.getBoolean("vid_comment_no_vote")) {
+                    callOriginal()
                 }
             }
+        }
+
+        // 评论区顶部的投票
+        "com.bilibili.app.comment.ext.widgets.CmtVoteWidget".toClass()
+            .method { name = "a" }.hook(YukiHookPriority.DEFAULT, replaceHook)
+        // 评论内容中站队信息
+        "com.bilibili.app.comm.comment2.phoenix.view.CommentMountWidget".toClass()
+            .method { name = "i0" }.hook(YukiHookPriority.DEFAULT, replaceHook)
     }
 }
