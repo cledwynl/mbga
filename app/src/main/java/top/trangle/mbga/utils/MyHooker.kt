77a355c3469e9bf9c06044abd225c628d35a6b04
Cledwyn Lew <me@trangle.top>
2024-06-26 22:23:06 +0800

    chore: formatKotlin

--- app/src/main/java/top/trangle/mbga/utils/MyHooker.kt
+++ app/src/main/java/top/trangle/mbga/utils/MyHooker.kt
@@ -1,7 +1,5 @@
 package top.trangle.mbga.utils
 
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.core.content.pm.PackageInfoCompat
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
 import com.highcapable.yukihookapi.hook.log.YLog
@@ -18,8 +16,9 @@ abstract class MyHooker : YukiBaseHooker() {
         val a = 1..2
         onAppLifecycle {
             onCreate {
-                val pkgInfo = appContext?.packageManager?.getPackageInfo(packageName, 0)
-                    ?: return@onCreate YLog.error("Unable to get host info, versionSpecifiedSubHook not hooked")
+                val pkgInfo =
+                    appContext?.packageManager?.getPackageInfo(packageName, 0)
+                        ?: return@onCreate YLog.error("Unable to get host info, versionSpecifiedSubHook not hooked")
                 val version = PackageInfoCompat.getLongVersionCode(pkgInfo)
                 if (version in versionRange) {
                     return@onCreate
